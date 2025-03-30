ce16a006772746c48400f8197dec7350e81adf57
JadenKim-dev <jaden.kim5179@gmail.com>
2025-03-30 17:27:45 +0900

    ♻️ Dynamic module initialization refactor

--- src/kafka/kafka.module.ts
+++ src/kafka/kafka.module.ts
@@ -1,4 +1,4 @@
-import { Module, OnApplicationShutdown, OnModuleInit } from '@nestjs/common';
+import { Global, Module, OnApplicationShutdown, OnModuleInit } from '@nestjs/common';
 import { ClientKafka, ClientsModule, Transport } from '@nestjs/microservices';
 import { ModuleRef } from '@nestjs/core';
 import { ConfigModule } from '@nestjs-library/config';
@@ -6,28 +6,27 @@ import { KAFKA_PRODUCER } from './kafka.symbol';
 import { KafkaConfigService } from './kafka-config.service';
 import { KafkaService } from './kafka.service';
 
-@Module({
-    imports: [
-        ConfigModule.forFeature(KafkaConfigService, {
-            global: true,
-        }),
-        ClientsModule.registerAsync([
-            {
-                name: KAFKA_PRODUCER,
-                useFactory: (configService: KafkaConfigService) => ({
-                    transport: Transport.KAFKA,
-                    options: {
-                        client: {
-                            brokers: [configService.broker],
-                        },
-                    },
-                }),
-                inject: [KafkaConfigService],
+const configModule = ConfigModule.forFeature(KafkaConfigService);
+const kafkaClientsModule = ClientsModule.registerAsync([
+    {
+        name: KAFKA_PRODUCER,
+        useFactory: (configService: KafkaConfigService) => ({
+            transport: Transport.KAFKA,
+            options: {
+                client: {
+                    brokers: [configService.broker],
+                },
             },
-        ]),
-    ],
+        }),
+        inject: [KafkaConfigService],
+        imports: [configModule],
+    },
+]);
+
+@Module({
+    imports: [configModule, kafkaClientsModule],
     providers: [KafkaConfigService, KafkaService],
-    exports: [KafkaService, KAFKA_PRODUCER],
+    exports: [KafkaService, kafkaClientsModule],
 })
 export class KafkaModule implements OnApplicationShutdown, OnModuleInit {
     constructor(private readonly moduleRef: ModuleRef) {}
